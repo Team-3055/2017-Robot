9cb575897336c414d91478ca0da8f3b5d197057f
Team3055 <connor.williams@austin.k12.mn.us>
2017-01-10 13:17:26 -0600

    Update to 2017 code

--- 2017 Robot/src/Robot.cpp
+++ 2017 Robot/src/Robot.cpp
@@ -1,69 +1,87 @@
+#include "WPILib.h"
 #include <iostream>
-#include <memory>
-#include <string>
+#include "math.h"
+using namespace std;
 
-#include <IterativeRobot.h>
-#include <LiveWindow/LiveWindow.h>
-#include <SmartDashboard/SendableChooser.h>
-#include <SmartDashboard/SmartDashboard.h>
-
-class Robot: public frc::IterativeRobot {
-public:
-	void RobotInit() {
-		chooser.AddDefault(autoNameDefault, autoNameDefault);
-		chooser.AddObject(autoNameCustom, autoNameCustom);
-		frc::SmartDashboard::PutData("Auto Modes", &chooser);
-	}
-
-	/*
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * GetString line to get the auto name from the text box below the Gyro.
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the
-	 * if-else structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
-	void AutonomousInit() override {
-		autoSelected = chooser.GetSelected();
-		// std::string autoSelected = SmartDashboard::GetString("Auto Selector", autoNameDefault);
-		std::cout << "Auto selected: " << autoSelected << std::endl;
-
-		if (autoSelected == autoNameCustom) {
-			// Custom Auto goes here
-		} else {
-			// Default Auto goes here
-		}
-	}
-
-	void AutonomousPeriodic() {
-		if (autoSelected == autoNameCustom) {
-			// Custom Auto goes here
-		} else {
-			// Default Auto goes here
-		}
-	}
-
-	void TeleopInit() {
-
-	}
-
-	void TeleopPeriodic() {
-
-	}
-
-	void TestPeriodic() {
-		lw->Run();
-	}
-
+class Robot: public IterativeRobot
+{
 private:
-	frc::LiveWindow* lw = LiveWindow::GetInstance();
-	frc::SendableChooser<std::string> chooser;
+	LiveWindow *lw = LiveWindow::GetInstance();
+	//SendableChooser *chooser;
 	const std::string autoNameDefault = "Default";
 	const std::string autoNameCustom = "My Auto";
 	std::string autoSelected;
+
+	//Control System
+	Joystick *xbox = new Joystick(0);
+	Joystick *xbox2 = new Joystick(1);
+
+	//Motor Controller Declarations
+	Talon *lDMotor = new Talon(0);
+	Spark *rDMotor = new Spark(1);
+
+
+	//Motor declarations
+	double lDrive, rDrive;
+	RobotDrive *robotDrive = new RobotDrive(lDMotor,rDMotor);
+
+	void RobotInit()
+	{
+		//chooser = new SendableChooser();
+		//chooser->AddDefault(autoNameDefault, (void*)&autoNameDefault);
+		//chooser->AddObject(autoNameCustom, (void*)&autoNameCustom);
+		//SmartDashboard::PutData("Auto Modes", chooser);
+	}
+
+
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
+	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
+	 * Dashboard, remove all of the chooser code and uncomment the GetString line to get the auto name from the text box
+	 * below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the if-else structure below with additional strings.
+	 * If using the SendableChooser make sure to add them to the chooser code above as well.
+	 */
+	void AutonomousInit()
+	{
+		//autoSelected = *((std::string*)chooser->GetSelected());
+		//std::string autoSelected = SmartDashboard::GetString("Auto Selector", autoNameDefault);
+		//std::cout << "Auto selected: " << autoSelected << std::endl;
+
+		//if(autoSelected == autoNameCustom){
+			//Custom Auto goes here
+		//} else {
+			//Default Auto goes here
+		//}
+	}
+
+	void AutonomousPeriodic()
+	{
+		//if(autoSelected == autoNameCustom){
+			//Custom Auto goes here
+		//} else {
+			//Default Auto goes here
+		//}
+	}
+
+	void TeleopInit()
+	{
+
+	}
+
+	void TeleopPeriodic()
+	{
+		 //Tank Drive
+		rDrive= xbox->GetRawAxis(1);
+		lDrive= xbox2->GetRawAxis(1);
+		robotDrive->TankDrive(lDrive, rDrive);
+	}
+
+	void TestPeriodic()
+	{
+		lw->Run();
+	}
 };
 
 START_ROBOT_CLASS(Robot)
