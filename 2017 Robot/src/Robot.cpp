ec307d2acf15fa2d9c4b7d22e8f315e73410bc4d
stiny861 <connor.williams@austin.k12.mn.us>
2017-01-11 14:51:34 -0600

    Added vision system code from example.

--- 2017 Robot/src/Robot.cpp
+++ 2017 Robot/src/Robot.cpp
@@ -1,6 +1,12 @@
 #include "WPILib.h"
 #include <iostream>
 #include "math.h"
+#include <thread>
+#include <CameraServer.h>
+#include <IterativeRobot.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/types.hpp>
 using namespace std;
 
 class Robot: public IterativeRobot
@@ -28,8 +34,45 @@ private:
 	double lDrive, rDrive;
 	RobotDrive *robotDrive = new RobotDrive(lDMotor,rDMotor);
 
+	//Vision Code
+	static void VisionThread() {
+			// Get the USB camera from CameraServer
+			cs::UsbCamera camera = CameraServer::GetInstance()->StartAutomaticCapture();
+			// Set the resolution
+			camera.SetResolution(640, 480);
+
+			// Get a CvSink. This will capture Mats from the Camera
+			cs::CvSink cvSink = CameraServer::GetInstance()->GetVideo();
+			// Setup a CvSource. This will send images back to the Dashboard
+			cs::CvSource outputStream = CameraServer::GetInstance()->
+					PutVideo("Rectangle", 640, 480);
+
+			// Mats are very memory expensive. Lets reuse this Mat.
+			cv::Mat mat;
+
+			while (true) {
+				// Tell the CvSink to grab a frame from the camera and put it
+				// in the source mat.  If there is an error notify the output.
+				if (cvSink.GrabFrame(mat) == 0) {
+					// Send the output the error.
+					outputStream.NotifyError(cvSink.GetError());
+					// skip the rest of the current iteration
+					continue;
+				}
+				// Put a rectangle on the image
+				rectangle(mat, cv::Point(100, 100), cv::Point(400, 400),
+						cv::Scalar(255, 255, 255), 5);
+				// Give the output stream a new image to display
+				outputStream.PutFrame(mat);
+			}
+		}
 	void RobotInit()
 	{
+		// We need to run our vision program in a separate Thread.
+				// If not, our robot program will not run
+				std::thread visionThread(VisionThread);
+				visionThread.detach();
+
 		chooser = new SendableChooser();
 		chooser->AddDefault(autoNameDefault, (void*)&autoNameDefault);
 		chooser->AddObject(autoNameCustom, (void*)&autoNameCustom);
